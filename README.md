4f95fc98760048b8df3bf43efa0d9b07a9e19fbc
Leo Picou <leo.picou@te.com>
2025-04-07 16:48:19 +0200

    implemented monitor page

--- README.md
+++ README.md
@@ -3,6 +3,12 @@ Process and decode fragmented LoRaWAN frames, receiving data over MQTT or HTTP, 
 
 Still in developpement phase.
 
+# Monitoring interface
+
+A Web Interface is available to monitor which frames are pending for which devices : 
+
+![monitor screenshot](images/monitor.png)
+
 # TODO :
 - HTTP Input/Output
 - Split main.py into relevant parts
--- src/app/js_fetcher.py
+++ src/app/js_fetcher.py
@@ -1,11 +1,11 @@
 import pythonmonkey as pm
-import sys
-import json
-
+import signal
 
 def js_worker(task_queue, result_queue):
     """Process that listens for tasks and executes JS functions."""
     decoder = pm.require("../submodules/ttn-decoder/TnnJsDecoder/TE_TtnDecoder.js")
+
+    signal.signal(signal.SIGINT, signal.SIG_IGN)
     while True:
         task = task_queue.get()  # Wait for task
         if task == "STOP":
@@ -18,5 +18,6 @@ def js_worker(task_queue, result_queue):
                 result_queue.put(str(result))
             else:
                 result_queue.put(f"Error: {func_name} not found in JS module")
+
         except Exception as e:
             result_queue.put(f"Error: {e}")
