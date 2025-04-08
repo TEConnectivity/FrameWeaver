bbc9868534e0ed566f351fcb06060b6091921ef2
Leo Picou <leo.picou@te.com>
2025-04-08 11:48:23 +0200

    Added Loriot MQTT, splitted main

--- src/app/js_fetcher.py
+++ src/app/js_fetcher.py
@@ -1,3 +1,4 @@
+from multiprocessing import Queue, Process
 import pythonmonkey as pm
 import signal
 
@@ -21,3 +22,28 @@ def js_worker(task_queue, result_queue):
 
         except Exception as e:
             result_queue.put(f"Error: {e}")
+
+
+
+
+####### Javascript interface #######
+
+def start_js_worker():
+    """Start the JS worker process and return the queues and process."""
+    task_queue = Queue()
+    result_queue = Queue()
+    
+    worker_process = Process(target=js_worker, args=(task_queue, result_queue))
+    worker_process.start()
+
+    return worker_process, task_queue, result_queue
+
+def stop_js_worker(task_queue: Queue, process: Process):
+    # Stop the fetcher process
+    task_queue.put("STOP")
+    process.join()
+
+def call_js_function(task_queue, result_queue, func_name, *args):
+    """Send a JS function call request and get the result."""
+    task_queue.put((func_name, args))  # Send function and args
+    return result_queue.get()  # Receive result
---
+++ src/app/self_broker.py
@@ -0,0 +1,26 @@
+
+######## MOSQUITTO #########
+
+# Run mosquitto as a subprocess
+import subprocess
+import logging
+
+
+logger = logging.getLogger(__name__)
+
+
+def start_mosquitto():
+    try:
+        # This will start Mosquitto in the background
+        process = subprocess.Popen(["mosquitto"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        logger.info("Self-hosted MQTT broker started.")
+        return process
+    except Exception as e:
+        logger.critical(f"Error starting Self-hosted MQTT Broker: {e}")
+        return None
+
+# Stop mosquitto (kill the process)
+def stop_mosquitto(process):
+    if process:
+        process.terminate()
+        logger.info("Self-hosted MQTT broker stopped.")
